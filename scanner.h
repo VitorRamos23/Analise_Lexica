75f642e8b13f1d23733a320555cb3c71a2510840
Vitor Ramos <148400880+VitorRamos23@users.noreply.github.com>
2025-11-09 16:57:07 -0400

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <cstdlib> // Para exit()
+#include <map>
 #include "token.h"
 
 class Scanner 
@@ -8,15 +9,18 @@ class Scanner
         string input;//Armazena o texto de entrada
         std::string::size_type pos;//Posição atual
         int line;
+        map<string, int> reservedWords;
     
     public:
     //Construtor
         Scanner(string);
+        void initReservedWords();
 
         int getLine();
     
         //Método que retorna o próximo token da entrada
-        Token* nextToken();        
+        Token* nextToken();
+        Token* peekToken(int k = 1);        
     
         //Método para manipular erros
         void lexicalError(string);
