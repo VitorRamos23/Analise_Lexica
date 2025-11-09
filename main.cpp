96e529194a449654bdbfdba9bd28482915636117
Vitor Ramos <148400880+VitorRamos23@users.noreply.github.com>
2025-11-09 16:58:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -62,4 +62,29 @@ int main(int argc, char* argv[])
     delete scanner;
     
     return 0;
+
+}
+
+
+
+
+
+
+#include "parser.h"
+
+int main(int argc, char* argv[]) 
+{
+    if (argc != 2)
+    {
+        cout << "Uso: ./xpp_compiler nome_arquivo.xpp\n";
+        return 1;
+    }
+
+    Parser* parser = new Parser(argv[1]);
+
+    parser->run();
+
+    delete parser;
+    
+    return 0;
 }
