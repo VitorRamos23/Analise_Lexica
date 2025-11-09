c4f41066809f753394d5f83848f491830d1897f1
Vitor Ramos <148400880+VitorRamos23@users.noreply.github.com>
2025-11-09 18:34:53 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 
 int main(int argc, char* argv[]) 
 {
+    //Verifica se foi executado corretamente
+    //Essa main espera receber o nome do arquivo a ser
+    //executado na linha de comando.
     if (argc != 2)
     {
-        cout << "Uso: ./xpp_compiler nome_arquivo.xpp\n";
+        // Mensagem de uso atualizada para o nome do executável da Etapa 2/3
+        cout<< "Uso: ./xpp_compiler nome_arquivo.xpp\n";
         return 1;
     }
 
     Parser* parser = new Parser(argv[1]);
 
     parser->run();
-
-    delete parser;
     
+    delete parser;
+
     return 0;
 }
-
